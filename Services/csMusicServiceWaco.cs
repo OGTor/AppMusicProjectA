8fd2d44991b37a293a3ef30b9417dcea8bf2d572
OGTor <112472401+OGTor@users.noreply.github.com>
2023-12-13 13:37:26 +0100

    G delen

--- Services/csMusicServiceWaco.cs
+++ Services/csMusicServiceWaco.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 using DbContext;
 using Models;
 using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
 
 namespace Services;
 
@@ -15,14 +17,34 @@ public class csMusicServiceWaco : IMusicService
     public async Task<int> Seed(int NrOfitems)
     {
         string uri = $"csadmin/seed?count={NrOfitems}";
+        
+        //Get the response from the API
+        var response = await _httpClient.GetAsync(uri);
+        
+        //If the response is not successful, throw an exception
+        response.EnsureSuccessStatusCode();
 
-        throw new NotImplementedException();
+        //Read the response as a string
+        var result = await response.Content.ReadAsStringAsync();
+
+        //Return the result as an integer
+        return JsonConvert.DeserializeObject<int>(result);
+        
     }
     public async Task<int> RemoveSeed()
     {
         string uri = $"csadmin/removeseed";
+        //Get the response from the API
+        var response = await _httpClient.GetAsync(uri);
 
-        throw new NotImplementedException();
+        //If the response is not successful, throw an exception
+        response.EnsureSuccessStatusCode();
+
+        //Read the response as a string
+        var result = await response.Content.ReadAsStringAsync();
+
+        //Return the result as an integer
+        return JsonConvert.DeserializeObject<int>(result);
     }
     #endregion
 
@@ -30,37 +52,60 @@ public class csMusicServiceWaco : IMusicService
     public async Task<List<csMusicGroup>> ReadMusicGroupsAsync(bool flat)
     {
         string uri = $"csmusicgroups/read?flat={flat}";
-
-        throw new NotImplementedException();
+        
+        var response = await _httpClient.GetAsync(uri);
+        
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        
+        return JsonConvert.DeserializeObject<List<csMusicGroup>>(result);
+        
+       
     }
 
     public async Task<csMusicGroup> ReadMusicGroupAsync(Guid id, bool flat)
     {
-
         string uri = $"csmusicgroups/readitem?id={id}&flat={flat}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csMusicGroup>(result);
+        
     }
 
     public async Task<csMusicGroup> UpdateMusicGroupAsync(csMusicGroupCUdto _src)
     {
         string uri = $"csmusicgroups/updateitem/{_src.MusicGroupId}";
-
-        throw new NotImplementedException();
+        var json = JsonConvert.SerializeObject(_src);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csMusicGroup>(result);
     }
 
     public async Task<csMusicGroup> CreateMusicGroupAsync(csMusicGroupCUdto _src)
     {
         string uri = $"csmusicgroups/createitem";
-
-        throw new NotImplementedException();
+        var json = JsonConvert.SerializeObject(_src);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csMusicGroup>(result);
+    
+        
     }
 
     public async Task<csMusicGroup> DeleteMusicGroupAsync(Guid id)
     {
         string uri = $"csmusicgroups/deleteitem/{id}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.DeleteAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csMusicGroup>(result);
+  
     }
     #endregion
 
@@ -68,36 +113,62 @@ public class csMusicServiceWaco : IMusicService
     public async Task<List<csAlbum>> ReadAlbumsAsync(bool flat)
     {
         string uri = $"csalbums/read?flat={flat}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<csAlbum>>(result);
+        
+      
     }
 
     public async Task<csAlbum> ReadAlbumAsync(Guid id, bool flat)
     {
         string uri = $"csalbums/readitem?id={id}&flat={flat}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csAlbum>(result);
+        
+        
     }
 
     public async Task<csAlbum> CreateAlbumAsync(csAlbumCUdto _src)
     {
+        
+        
         string uri = $"csalbums/createitem";
+                var json = JsonConvert.SerializeObject(_src);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        
+        return JsonConvert.DeserializeObject<csAlbum>(result);
 
-        throw new NotImplementedException();
+       
+        
     }
 
     public async Task<csAlbum> UpdateAlbumAsync(csAlbumCUdto _src)
     {
         string uri = $"csalbums/updateitem/{_src.AlbumId}";
-
-        throw new NotImplementedException();
+        var json = JsonConvert.SerializeObject(_src);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csAlbum>(result);
+        
     }
 
     public async Task<csAlbum> DeleteAlbumAsync(Guid id)
     {
         string uri = $"csalbums/deleteitem/{id}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.DeleteAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csAlbum>(result);
+        
     }
     #endregion
 
@@ -105,37 +176,58 @@ public class csMusicServiceWaco : IMusicService
     public async Task<List<csArtist>> ReadArtistsAsync(bool flat)
     {
         string uri = $"csartists/read?flat={flat}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<csArtist>>(result);
+        
+    
+        
     }
 
     public async Task<csArtist> ReadArtistsync(Guid id, bool flat)
     {
         string uri = $"csartists/readitem?id={id}&flat={flat}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csArtist>(result);
+        
     }
 
     //Upsert = Update or Insert
     public async Task<csArtist> UpsertArtistAsync(csArtistCUdto _src)
     {
         string uri = $"csartists/upsertitem";
-
-        throw new NotImplementedException();
+        var json = JsonConvert.SerializeObject(_src);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csArtist>(result);
+        
     }
 
     public async Task<csArtist> UpdateArtistAsync(csArtistCUdto _src)
     {
         string uri = $"csartists/updateitem/{_src.ArtistId}";
-
-        throw new NotImplementedException();
+        var json = JsonConvert.SerializeObject(_src);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(uri, data);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<csArtist>(result);
+        
     }
 
     public async Task<csArtist> DeleteArtistAsync(Guid id)
     {
         string uri = $"csartists/deleteitem/{id}";
-
-        throw new NotImplementedException();
+        var response = await _httpClient.DeleteAsync(uri);
+    response.EnsureSuccessStatusCode();
+    var result = await response.Content.ReadAsStringAsync();
+    return JsonConvert.DeserializeObject<csArtist>(result);
+    
     }
     #endregion
 
@@ -146,6 +238,7 @@ public class csMusicServiceWaco : IMusicService
         _httpClient = httpClientFactory.CreateClient(name: "MusicWebApi");
     }
 }
+//when i run this it shows 0 albums on music group page. but when i click on the album page it shows the albums.
 
 
 
